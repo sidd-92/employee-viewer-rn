45a01fe8801ecb1e845404a1a3d114777e2ae6a9
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-07-25 19:16:04 +0530

    Tailwind Setup

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { tailwind } from "./tailwind";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ fontWeight: "bold", fontSize: 28, textAlign: "center" }}>
-        Open up App.js to start working on your app!
+    <View
+      style={tailwind("flex-1 bg-gray-50 items-center justify-center m-10")}
+    >
+      <Text style={tailwind("text-center font-bold text-4xl text-red-600")}>
+        Title
       </Text>
+      <Text style={tailwind("text-center font-thin text-2xl my-10")}>
+        Thin Text
+      </Text>
+      <View
+        style={[
+          tailwind("bg-red-100 h-10 items-center justify-center"),
+          { width: 200 },
+        ]}
+      >
+        <Text style={tailwind("text-black text-center")}>Hello</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
